ac05289414e9e22718986afd25f43e72b478934b
Felix Zhu <fz288@login-q-4.data.cluster>
2024-06-04 01:27:53 +0100

    .

--- src/models/rerank/mores_model.py
+++ src/models/rerank/mores_model.py
@@ -72,6 +72,8 @@ class MORESSym(nn.Module):
 
     def forward(self, qry, doc, qry_mask, cross_mask, attention_adj = None):
 
+        qry_mask = self.get_extended_attention_mask(qry_mask, qry_mask.size(), qry.device)
+        cross_mask = self.get_extended_attention_mask(cross_mask, doc.size(), qry.device)
 
         hidden_states = qry
         for i, ib_layer in enumerate(self.interaction_module):
@@ -80,7 +82,6 @@ class MORESSym(nn.Module):
                 attention_mask=qry_mask,
                 encoder_hidden_states=doc,
                 encoder_attention_mask=cross_mask,
-                output_attentions=self.bert.config.output_attentions,
             )
             hidden_states = layer_outputs[0]
 
@@ -91,3 +92,24 @@ class MORESSym(nn.Module):
         logits2 = self.classifier2(cls_reps)
 
         return logits1, logits2
+    
+    def get_extended_attention_mask(self, attention_mask, input_shape, device):
+        """
+        Make an extended attention mask for attention masking.
+        """
+        # Prepare the attention mask
+        if attention_mask.dim() == 3:
+            extended_attention_mask = attention_mask[:, None, :, :]
+        elif attention_mask.dim() == 2:
+            extended_attention_mask = attention_mask[:, None, None, :]
+        else:
+            raise ValueError("Wrong shape for attention_mask (shape {})".format(attention_mask.shape))
+
+        # Since attention_mask is 1.0 for positions we want to attend and 0.0 for
+        # masked positions, this operation will create a tensor which is 0.0 for
+        # positions we want to attend and -10000.0 for masked positions.
+        # Since we are adding it to the raw scores before the softmax, this is
+        # effectively the same as removing these entirely.
+        extended_attention_mask = extended_attention_mask.to(dtype=torch.float32)  # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+        return extended_attention_mask
